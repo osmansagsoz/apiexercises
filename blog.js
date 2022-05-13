0f7bd5b5fd62a716765d77d90fea0663f4c3c891
Osman Sagsoz <osmansagsoz1@gmail.com>
2022-05-13 21:39:03 +0300

    post req

--- blog.js
+++ blog.js
@@ -1,5 +1,5 @@
 const baseEndPoint = 'https://jsonplaceholder.typicode.com/posts/';
-document.querySelector('button').addEventListener('click', fetchPosts);
+document.querySelector('.get').addEventListener('click', fetchPosts);
 
 async function fetchPosts() {
   const query = document.querySelector('input').value;
@@ -10,3 +10,25 @@ async function fetchPosts() {
   document.querySelector('h2').innerText = data.title;
   document.querySelector('.explanation').innerText = data.body;
 }
+
+const postTitle = document.querySelector('h3').innerText;
+const postBody = document.querySelector('.postbody').innerText;
+const sendButton = document.querySelector('.send');
+
+function sendPosts() {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: postTitle,
+      body: postBody,
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+sendButton.addEventListener('click', sendPosts);
